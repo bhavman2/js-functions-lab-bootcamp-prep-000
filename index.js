2a666975bd78b5664b89f5a216d5ea766924d87f
Bhavik Raval <bhavman2@gmail.com>
2018-02-14 02:51:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(){
+  return "Happy holidays!"
+}
+
+function happyHolidayTo(){
+  return "Happy holidays!"
+}
