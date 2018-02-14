751bf99bdf26d2270e0b3ca59b54a0ff06978993
Bhavik Raval <bhavman2@gmail.com>
2018-02-14 02:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayTo(holiday,days){
-  return `It's ${days} days until ${holiday}!`;
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`;
 }
