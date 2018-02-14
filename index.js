4b44f058247ed44d9221695fd6cab962de131c74
Bhavik Raval <bhavman2@gmail.com>
2018-02-14 02:53:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday,days){
-  return `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`;
 }
